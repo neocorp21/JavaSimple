16c1affe5f0b360b4edc1e7bb1dfa60ff49d9460
Alberto <alberto.limas.contractor@bbva.com>
2025-05-03 18:07:13 -0500

    g

---
+++ src/main/java/com/example/demojenkisfile/DemoJenkisFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojenkisfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkisFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenkisFileApplication.class, args);
+    }
+
+}
